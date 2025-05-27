f1be364679476f253a9325f7bc3607817fb2836f
Muhammad Tariq <m.tariq@iub.edu.pk>
2025-05-27 16:59:23 +0500

    Assets, themes, landing page, menus etc

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.landing');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('/users', UserController::class)->only('index');
+});
+
