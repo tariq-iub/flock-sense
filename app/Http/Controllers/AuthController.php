2a05d13e789a97efc352265224e2eaa71900bb6f
Muhammad Tariq <m.tariq@iub.edu.pk>
2025-05-29 22:01:00 +0500

    Auth controller added, Api routes updated

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        //
+    }
+
+    public function logout(Request $request)
+    {
+        //
+    }
+
+    public function register(Request $request)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\FarmController;
+use App\Http\Controllers\Api\V1\ShedController;
+use App\Http\Controllers\Api\V1\DeviceController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResources([
+        'users' => UserController::class,
+        'farms' => FarmController::class,
+        'sheds' => ShedController::class,
+        'devices' => DeviceController::class,
+    ]);
+});
