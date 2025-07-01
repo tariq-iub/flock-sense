003613b439337692904014a2eb93b3e176c88e1a
System Analyst <m.tariq@iub.edu.pk>
2025-07-01 20:43:42 +0500

    User index function added for web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'users' => UserController::class,
+]);
