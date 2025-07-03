6fb6f73e47344b43e55e1781bd93384c1a734e58
Abdullah Abid <abdrps2004@gmail.com>
2025-07-03 23:17:12 +0500

    Created many-to-many relation between sheds and devices with a unique constraint added on device id, this way achieved many-to-one relation. Now working fine, previously it wasn't.

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,13 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Device extends Model
 {
     protected $fillable = ['serial_no', 'firmware_version', 'capabilities'];
 
-    public function shed() : HasOneThrough
+    public function sheds(): BelongsToMany
     {
-        return $this->hasOneThrough(Shed::class, ShedDevice::class);
+        return $this->belongsToMany(Shed::class, 'shed_devices')
+            ->withPivot('link_date')
+            ->withTimestamps();
     }
+
 }
--- app/Models/Shed.php
+++ app/Models/Shed.php
@@ -5,18 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Shed extends Model
 {
     protected $fillable = ['farm_id', 'name', 'capacity'];
 
-    public function farm() : BelongsTo
+    public function farm(): BelongsTo
     {
         return $this->belongsTo(Farm::class);
     }
 
-    public function devices() : HasManyThrough
+    public function devices(): BelongsToMany
     {
-        return $this->hasManyThrough(Device::class, ShedDevice::class);
+        return $this->belongsToMany(Device::class, 'shed_devices')
+            ->withPivot('link_date')
+            ->withTimestamps();
     }
+
 }
