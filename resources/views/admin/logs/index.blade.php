474a4b5d9edbd1ca023b6021ce7688d8908de05f
System Analyst <m.tariq@iub.edu.pk>
2025-07-17 23:18:14 +0500

    ProductionLog Graphs

--- resources/views/admin/logs/index.blade.php
+++ resources/views/admin/logs/index.blade.php
@@ -11,25 +11,15 @@
                 </div>
             </div>
             <ul class="table-top-head">
+                <li class="me-2">
+                    <a data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Refresh" data-bs-original-title="Refresh" aria-describedby="tooltip189061"><i class="ti ti-refresh"></i></a>
+                </li>
                 <li>
                     <a data-bs-toggle="tooltip" data-bs-placement="top" title="Collapse" id="collapse-header">
                         <i class="ti ti-chevron-up"></i>
                     </a>
                 </li>
             </ul>
-            <div class="page-btn">
-                <a href="javascript:void(0);" class="btn btn-primary" id="excelExportBtn"
-                   data-bs-toggle="tooltip" data-bs-placement="top"
-                   aria-label="Excel" data-bs-original-title="Excel">
-                    <i class="ti ti-file-excel me-1"></i>Excel
-                </a>
-
-                <a href="javascript:void(0);" id="print" class="btn btn-primary"
-                   data-bs-toggle="tooltip" data-bs-placement="top"
-                   aria-label="Print" data-bs-original-title="Print">
-                    <i class="ti ti-printer me-1"></i>Print
-                </a>
-            </div>
         </div>
 
         <div class="container-fluid">
@@ -64,45 +54,99 @@
                 @endif
             </div>
         </div>
-
         <div class="card">
-            <div class="card-header">
-                {{-- Filter Form --}}
+            <div class="card-body pb-1">
                 <form id="filterForm" method="GET" action="{{ route('productions.index') }}" class="row g-2 mb-3">
-                    <div class="col-md-3">
-                        <select id="farmSelect" class="select2">
-                            <option value="">Select Farm</option>
-                            @foreach($farms as $farm)
-                                <option value="{{ $farm->id }}" {{ (isset($farmId) && $farmId == $farm->id) ? 'selected' : '' }}>
-                                    {{ $farm->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-3">
-                        <select name="filter[shed_id]" id="shedSelect" class="select2" disabled>
-                            <option value="">Select Shed</option>
-                        </select>
-                    </div>
-                    <div class="col-md-3">
-                        <select name="filter[flock_id]" id="flockSelect" class="select2" disabled>
-                            <option value="">Select Flock</option>
-                        </select>
-                    </div>
-                    <div class="col-md-3">
-                        <button type="submit" class="btn btn-success float-end" id="showLogsBtn" disabled>
-                            Show Logs
-                        </button>
+                    <div class="row align-items-end">
+                        <div class="col-lg-10">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label">Select Farm</label>
+                                        <select id="farmSelect" class="select2">
+                                            <option value="">Select Farm</option>
+                                            @foreach($farms as $farm)
+                                                <option value="{{ $farm->id }}" {{ (isset($farmId) && $farmId == $farm->id) ? 'selected' : '' }}>
+                                                    {{ $farm->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label">Select Shed</label>
+                                        <select name="filter[shed_id]" id="shedSelect" class="select2" disabled>
+                                            <option value="">Select Shed</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label">Select Flock</label>
+                                        <select name="filter[flock_id]" id="flockSelect" class="select2" disabled>
+                                            <option value="">Select Flock</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="mb-3">
+                                <button class="btn btn-success w-100" type="submit" id="showLogsBtn" disabled>
+                                    Get Production Logs
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
+        </div>
 
-            <div class="card-body">
-                Graphs
+        <div class="card no-search">
+            <div class="card-header d-flex align-items-center justify-content-between flex-wrap row-gap-3">
+                <div>
+                    <h4>Production Logs</h4>
+                </div>
+                <ul class="table-top-head">
+                    <li class="me-2">
+                        <a href="javascript:void(0);" class="btn btn-outline-success" id="excelExportBtn"
+                           data-bs-toggle="tooltip" data-bs-placement="top"
+                           aria-label="Excel" data-bs-original-title="Excel">
+                            <i class="ti ti-file-excel fs-16"></i>
+                        </a>
+                    </li>
+                    <li class="me-2">
+                        <a href="javascript:void(0);" id="print" class="btn btn-outline-success"
+                           data-bs-toggle="tooltip" data-bs-placement="top"
+                           aria-label="Print" data-bs-original-title="Print">
+                            <i class="ti ti-printer fs-16"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
 
-            <div class="card-body printArea">
-                <div class="table-responsive">
+            <div class="card-body p-0">
+
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                Graph Trends
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample" style="">
+                            <div class="accordion-body">
+                                <div class="row">
+                                    <div class="col-md-6"></div>
+                                    <div class="col-md-6"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="table-responsive printArea">
                     <table class="table table-nowrap table-hover mb-0">
                         <thead class="thead-light">
                         <tr>
