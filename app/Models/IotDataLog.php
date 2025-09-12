dac6f55da6eadd5ed3d76ba08c47ab450f5a638f
Abdullah Abid <abdrps2004@gmail.com>
2025-09-13 03:06:25 +0500

    scheduled the services

--- app/Models/IotDataLog.php
+++ app/Models/IotDataLog.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class IotDataLog extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'shed_id',
+        'device_id',
+        'parameter',
+        'min_value',
+        'max_value',
+        'avg_value',
+        'record_time',
+        'time_window',
+    ];
 }
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('weather:fetch')->everyFifteenMinutes();
+
+Schedule::command('iot:aggregate-data')->dailyAt('00:05');
