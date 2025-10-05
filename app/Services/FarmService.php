17d16bcb018ec9cb37436a3f63ffe77504d607db
Abdullah Abid <abdrps2004@gmail.com>
2025-10-05 23:13:49 +0500

    used the updated get sensor data function in the farm service

--- app/Services/FarmService.php
+++ app/Services/FarmService.php
@@ -101,7 +101,8 @@ class FarmService
         foreach ($farms as $farm) {
             foreach ($farm->sheds as $shed) {
                 foreach ($shed->devices as $device) {
-                    $data = $this->dynamo->getLatestSensorData([$device->id]); // correct argument order
+                    $data = $this->dynamo->getSensorData([$device->id], null, null, true); // correct argument order
+//                    $data = $this->dynamo->getLatestSensorData([$device->id]); // correct argument order
                     $device->latest_sensor_data = $data[$device->id] ?? null;
                 }
             }
