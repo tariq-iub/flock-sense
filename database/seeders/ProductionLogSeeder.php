e20b9e050ccfff5d8a54f6a4df9735b661253a1a
Abdullah Abid <abdrps2004@gmail.com>
2025-10-20 18:22:38 +0500

    commited

--- database/seeders/ProductionLogSeeder.php
+++ database/seeders/ProductionLogSeeder.php
@@ -43,8 +43,8 @@ class ProductionLogSeeder extends Seeder
             $lastNetCount = $lastLog ? $lastLog->net_count : $flock->chicken_count;
 
             // 4. Calculate net_count and livability
-            $day_mortality = (int) $row[3];
-            $night_mortality = (int) $row[4];
+            $day_mortality = (int)$row[3];
+            $night_mortality = (int)$row[4];
             $net_count = $lastNetCount - ($day_mortality + $night_mortality);
 
             $livability = $flock->chicken_count > 0
@@ -57,21 +57,21 @@ class ProductionLogSeeder extends Seeder
             // 6. Create ProductionLog
             ProductionLog::firstOrCreate([
                 'production_log_date' => $production_log_date,
-                'shed_id'             => (int) $row[1],
-                'flock_id'            => $flock_id,
-                'age'                 => $age,
+                'shed_id' => (int)$row[1],
+                'flock_id' => $flock_id,
+                'age' => $age,
                 'day_mortality_count' => $day_mortality,
-                'night_mortality_count'=> $night_mortality,
-                'net_count'           => $net_count,
-                'livability'          => $livability,
-                'day_feed_consumed'   => (float) $row[5],
-                'night_feed_consumed' => (float) $row[6],
-                'day_water_consumed'  => (float) $row[7],
-                'night_water_consumed'=> (float) $row[8],
-                'is_vaccinated'       => (bool) $row[9],
-                'day_medicine'        => $row[10] ?? '',
-                'night_medicine'      => $row[11] ?? '',
-                'user_id'             => 1,
+                'night_mortality_count' => $night_mortality,
+                'net_count' => $net_count,
+                'livability' => $livability,
+                'day_feed_consumed' => (float)$row[5],
+                'night_feed_consumed' => (float)$row[6],
+                'day_water_consumed' => (float)$row[7],
+                'night_water_consumed' => (float)$row[8],
+                'is_vaccinated' => (bool)$row[9],
+                'day_medicine' => $row[10] ?? '',
+                'night_medicine' => $row[11] ?? '',
+                'user_id' => 1,
             ]);
         }
     }
