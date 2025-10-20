b8c3a17a4648010be7d51627b680c057d8bd5251
Abdullah Abid <abdrps2004@gmail.com>
2025-10-20 23:20:27 +0500

    updated seeders for production log and weight log

--- database/seeders/WeightLogSeeder.php
+++ database/seeders/WeightLogSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Flock;
+use App\Models\Shed;
 use App\Services\WeightLogService;
 use App\Models\ProductionLog;
 use Maatwebsite\Excel\Facades\Excel;
@@ -20,6 +22,9 @@ class WeightLogSeeder extends Seeder
         $weightRows = Excel::toArray([], public_path('assets/data/WeightData.xlsx'))[0];
         $timezone = 'Asia/Karachi';
 
+        $shed_ids = Shed::all()->pluck('id')->toArray();
+        $flock_ids = Flock::all()->pluck('id')->toArray();
+
         foreach ($weightRows as $index => $row) {
             if ($index == 0) continue; // skip header row
 
@@ -28,7 +33,16 @@ class WeightLogSeeder extends Seeder
                 ExcelDate::excelToDateTimeObject($row[0])
             )->setTimezone($timezone);
 
-            // 2. Find the related Production Log (by date, flock, or shed as needed)
+            // 2. Get related flock and opening count
+            $flock_id = (int)$row[2];
+            $shed_id = (int)$row[3];
+
+            $flock = Flock::find($flock_id);
+            $shed = Shed::find($shed_id);
+
+            if (!$shed or !$flock) continue;
+
+            // 3. Find the related Production Log (by date, flock, or shed as needed)
             $log = ProductionLog::whereDate('production_log_date', $production_log_date->toDateString())->first();
 
             $service->createOrUpdateWeightLog(
